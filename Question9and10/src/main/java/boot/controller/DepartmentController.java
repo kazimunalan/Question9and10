169079a2b6ce27c73cd40bd21ce72b982b967731
kazimunalan <kazimunalan@gmail.com>
2016-11-29 09:57:13 +0200

    Update DepartmentController.java

--- Question9and10/src/main/java/boot/controller/DepartmentController.java
+++ Question9and10/src/main/java/boot/controller/DepartmentController.java
@@ -15,13 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import boot.model.Department;
 import boot.model.Employee;
 import boot.service.DepartmentService;
+import boot.service.EmployeeService;
 
 @Controller
 public class DepartmentController {
 	
 	@Autowired
 	private DepartmentService departmentService;
-	
+	@Autowired
+	private EmployeeService employeeService;
 
 
 	
@@ -29,6 +31,7 @@ public class DepartmentController {
 	public String allTasks(HttpServletRequest request){
 		request.setAttribute("tasks", departmentService.findAll());
 		request.setAttribute("mode", "MODE_DEPARTMENT");
+		
 		return "index";
 	}
 	
@@ -41,14 +44,18 @@ public class DepartmentController {
 	
 	@GetMapping("/new-department")
 	public String newTask(HttpServletRequest request){
+		request.setAttribute("employee", employeeService.findAll());
 		request.setAttribute("mode", "MODE_NEW_DEPARTMENT");
 		return "index";
 	}
 	
 	@PostMapping("/save-department")
-	public String saveTask(@ModelAttribute Department task, BindingResult bindingResult, HttpServletRequest request){
+	public String saveTask(@ModelAttribute Department task, @ModelAttribute Employee employee, BindingResult bindingResult, HttpServletRequest request){
 		//task.setDateCreated(new Date());
+		Employee emp = employeeService.findTask(employee.getId());
+		task.setEmployee(emp);
 		departmentService.save(task);
+		
 		request.setAttribute("tasks", departmentService.findAll());
 		request.setAttribute("mode", "MODE_DEPARTMENT");
 		return "index";

3c6170015ffa5dd5c5bd69b381f2fa39d58bea83
kazimunalan <kazimunalan@gmail.com>
2016-11-29 09:56:23 +0200

    Update Employee.java

--- Question9and10/src/main/java/boot/model/Employee.java
+++ Question9and10/src/main/java/boot/model/Employee.java
@@ -2,37 +2,53 @@ package boot.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-//entity anotasyonu veritabaninda tablo olusturulmasini saglar
-@Entity(name="t_employee")//veritabaninda olusacak tablonun ismi "t_user" 'dir
+@Entity(name="t_employee")
 public class Employee implements Serializable{
 	
-	@Id// tablodaki user instance i icin id saglanmistir
-	@GeneratedValue(strategy=GenerationType.AUTO)//id otamatik olarak bir bir artirilir.
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String employee_name;
 	private String employee_surname;
 	private int employee_salary;
+	
+	@OneToMany(mappedBy = "employee")
+	private List<Department> department;
 
 	
 	public Employee(){}
 	
 
 
-	public Employee(int id, String employee_name, String employee_surname, int employee_salary) {
+
+
+	public Employee(int id, String employee_name, String employee_surname, int employee_salary,
+			List<Department> department) {
 		super();
 		this.id = id;
 		this.employee_name = employee_name;
 		this.employee_surname = employee_surname;
 		this.employee_salary = employee_salary;
+		this.department = department;
 	}
+
+
+
+
+
+
 
 
 
@@ -78,6 +94,23 @@ public class Employee implements Serializable{
 	public void setEmployee_salary(int employee_salary) {
 		this.employee_salary = employee_salary;
 	}
+
+	
+
+
+	public List<Department> getDepartment() {
+		return department;
+	}
+
+
+
+
+
+	public void setDepartment(List<Department> department) {
+		this.department = department;
+	}
+
+
 
 
 
